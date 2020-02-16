f8ce8c159545a0fa3afa9c0b7eb435c3f140969a
Billy Meis <54307189+TheBillyIsMe@users.noreply.github.com>
2020-02-16 15:49:55 -0500

    use ssl
    Signed-off-by: Billy Meis <54307189+TheBillyIsMe@users.noreply.github.com>

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ const https = require('https');
 const inquire = require('./app_routes/index');
 const app = express();
 
+const options = {
+	ca: [ fs.readFileSync('../xampp/apache/ssl/primestudios_co-1.ca'), fs.readFileSync('../xampp/apache/ssl/primestudios_co-2.ca'), fs.readFileSync('../xampp/apache/ssl/primestudios_co-3.ca') ],
+	cert: fs.readFileSync('../xampp/apache/ssl/primestudios_co.crt'),
+	key: fs.readFileSync('../xampp/apache/ssl/privatekey.pem')
+};
+
 app.port = appConfig.port;
 
 app.use(cors);
@@ -15,6 +21,8 @@ app.use(express.urlencoded({extended: false}));
 app.use(helmet());
 
 app.use('/', inquire);
+
+const server = https.createServer(options, app);
 
 app.listen(app.port, function appListener () {
 	console.log(`Prime Studios Contact API listening on port ${app.port}.`);
